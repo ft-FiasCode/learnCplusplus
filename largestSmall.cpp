38e5548a830bed62a86af8b8ca2364777de96a89
ft-FiasCode <amin04.edu@gmail.com>
2025-11-12 03:12:23 -0800

    finding largest and small number.

---
+++ largestSmall.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    cout << "Enter array size ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << "number" << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int lar = arr[0];
+    int sam = arr[0]; 
+    
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] > lar) {
+            lar = arr[i]; 
+        }
+        if (arr[i] < sam) {
+            sam = arr[i]; 
+        }
+    }  
+    cout << "The lar number in the array is: " << lar << endl;
+    cout << "The sam number in the array is: " << sam << endl;
+
+    return 0;
+}
